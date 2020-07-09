b462f7ec93045423a74f9892ee7098e3702ae65c
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-07-09 19:54:58 -0400

    get data from the trivia api database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Question from "./components/Question";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        //manipulate the data structure
+        //{question:.... answers:... selections:....(randomize selections)}
+        setQuestions(result.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Quiz App</h1>
       </header>
+      {questions && questions.map((item, index) => item.question)}
+      <Question />
     </div>
   );
 }
